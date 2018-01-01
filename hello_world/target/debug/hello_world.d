b37ee8fc4b021fd60f65dbb8365a6d7a1e484e94
Morichan <nintendo698@gmail.com>
2018-01-01 15:42:31 +0900

    learn-rust

---
+++ hello_world/target/debug/hello_world.d
@@ -0,0 +1,1 @@
+D:\Documents\workspace\learnRust\target\debug\hello_world.exe: D:\Documents\workspace\learnRust\src\main.rs
---
+++ hello_world/target/release/hello_world.d
@@ -0,0 +1,1 @@
+D:\Documents\workspace\learnRust\target\release\hello_world.exe: D:\Documents\workspace\learnRust\src\main.rs
